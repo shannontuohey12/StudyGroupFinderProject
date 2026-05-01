a37969eb0267b07401f39e978256c69ed36a9bb6
Genesis <jugen503@gmail.com>
2026-04-30 22:13:15 -0400

    Factory changes, still in progress.

--- SpringBootEx/src/main/java/com/SprintBootEx/SpringBootEx/SpringBootExApplication.java
+++ SpringBootEx/src/main/java/com/SprintBootEx/SpringBootEx/SpringBootExApplication.java
@@ -12,62 +12,47 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringBootExApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExApplication.class, args);
+    }
 
-	@Bean
+    @Bean
     CommandLineRunner init(UserRepository userRepo, StudyGroupRepository groupRepo) {
         return args -> {
 
             // Create User
-            User user = new User();
-            user.setName("Shannon");
-            user.setEmail("shannon@test.com");
-            user.setMajor("Computer Science");
-			user.setPassword("password");
-            userRepo.save(user);
+            User user = userRepo.findByEmail("shannon@test.com").orElseGet(() -> {
+                User newUser = new User();
+                newUser.setName("Shannon");
+                newUser.setEmail("shannon@test.com");
+                newUser.setMajor("Computer Science");
+                newUser.setPassword("password");
+                return userRepo.save(newUser);
+            });
 
-            // Create Group
-            StudyGroup group = new StudyGroup();
-            group.setSubject("CSII");
-            group.setTitle("Algorithms Help");
-            group.setDate("2026-10-10");
-            group.setTime("18:00");
-            groupRepo.save(group);
+            if (groupRepo.count() == 0) {
+                StudyGroup group = new StudyGroup();
+                group.setSubject("CSII");
+                group.setTitle("Algorithms Help");
+                group.setDate("2026-10-10");
+                group.setTime("18:00");
+                groupRepo.save(group);
 
-            StudyGroup group2 = new StudyGroup();
-            group2.setSubject("Calculus I");
-            group2.setTitle("Derivatives Study Group");
-            group2.setDate("2026-10-10");
-            group2.setTime("18:00");
-            groupRepo.save(group2);
+                StudyGroup group2 = new StudyGroup();
+                group2.setSubject("Calculus I");
+                group2.setTitle("Derivatives Study Group");
+                group2.setDate("2026-10-10");
+                group2.setTime("18:00");
+                groupRepo.save(group2);
 
-            StudyGroup group3 = new StudyGroup();
-            group3.setSubject("English 170");
-            group3.setTitle("Writing Workshop");
-            group3.setDate("2026-10-10");
-            group3.setTime("18:00");
-            groupRepo.save(group3);
+                StudyGroup group3 = new StudyGroup();
+                group3.setSubject("English 170");
+                group3.setTitle("Writing Workshop");
+                group3.setDate("2026-10-10");
+                groupRepo.save(group3);
 
-            // Join group
-            user.getGroups().add(group);
-            user.getGroups().add(group2);
-            user.getGroups().add(group3);
-
-            group.getUsers().add(user);
-            group2.getUsers().add(user);
-            group3.getUsers().add(user);
-
-            userRepo.save(user);
-            groupRepo.save(group);
-            groupRepo.save(group2);
-            groupRepo.save(group3);
-
-            System.out.println("🔥 Seed data created!");
-
-		};
-
-	}
-
+                System.out.println("Seed data created!");
+            }
+        };
+    }
 }
